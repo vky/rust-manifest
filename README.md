c93fba6242f4de2f8affae646684ddfdc9cf9cc3
Vijay Korapaty <rust-manifest@vky.me>
2013-12-19 21:31:54 -0800

    The script works.
    Figured out callbacks sufficiently to get the script working. Obtained
    all of the search results for Rust repos created after 01-01-2010, and
    wrote the results to two files: completed_repos.json and
    simple_repos.json.
    
    These are only the repositories that Github considers to be Rust
    repositories. Should come up with better search criteria in the future.

--- README.md
+++ README.md
@@ -2,13 +2,27 @@
 
 An automatically updating list of Rust projects on Github.
 
+## TODO
+
+* Present collected data in nice HTML
+* How to store data
+* How to use OAuth
+* Allow people to add Rust projects not found by search
+* Rewrite in Rust
+
+## Presentation description
+
 Seperate out projects that have been updated in the past month and those that have not.
+
+Active (updated since most recent Rust versioned release)
+
+Inactive (last update before most recent Rust versioned release)
 
 project name | author | build status | last commit / date | [ docs ] | forks | star
 
 * project name should link to the project page,
 * author to the author's github page
-* build status to the build status
+* automatically insert travis ci link for repo even if it doesn't exist.
 * last commit to the commits page
 * Fork and star do what they would do if one is logged into github. Show the number and icon for both.
 * docs will do nothing. I may attempt to run rustdoc on projects automatically/manually, but do that after the core listing processes work.
@@ -16,14 +30,16 @@ project name | author | build status | last commit / date | [ docs ] | forks | s
 
 One big table.
 
-Once the list is in place and works, perhaps consider some kind of ranking?
+Once the list is in place and works, consider the following:
 
-======
+* tagging projects
+* average project rating (1-5 rust icons)
 
-astavonin/RustyKext is an example Rust project that won't be found with the criteria of a Rust language project created after 2010 because it is majority C. Need a way for people to add projects to track.
+## Problems
 
-======
+astavonin/RustyKext is an example Rust project that won't be found with the criteria of a Rust language project created after 2010 because it is majority C. Need a way for people to add projects not found by search.
 
+## First commit in Rust repo, search criteria setup around this
 
 commit c01efc669f09508b55eced32d3c88702578a7c3e
 Author: Graydon Hoare <graydon@mozilla.com>
--- test.js
+++ test.js
@@ -1,29 +1,24 @@
 var request = require('request');
 var qs = require('querystring');
+var url = require('url');
+var fs = require('fs');
 
-var auth = {
-    username: '',
-    password: ''
-};
+var request = request.defaults({
+    auth: {
+        username: '',
+        password: ''
+    },
+    headers: {
+        "User-Agent": "Rust-Manifest"
+    }
+});
 
-var github = "https://api.github.com";
-var method = "/search/repositories";
+var github_api = "https://api.github.com/search/repositories";
+var searchCriteria = "created:>2010-01-01 language:Rust";
+var db = [];
 
-// Can only get 100 items max per request.
-// In the headers, there's a link attribute which lets you know where you
-// are in the set of results.
-var query = {
-    q: "created:>2010-01-01 language:Rust",
-    per_page: 100,
-    page: 10
-};
-
-function callback (err, response, body, thing) {
-    //console.log(err);
-    console.log(response.headers.link);
-    //console.log(response);
-
-    function getRepoAttributes (element) {
+function processSearchResults (err, response, body) {
+    function getSimpleRepoAttributes (element) {
         var project = {
             name: element.name,
             url: element.owner.html_url,
@@ -35,25 +30,106 @@ function callback (err, response, body, thing) {
         return project;
     }
 
+    function nextPage (link) {
+        // Split the link string on ">;" or '",'.
+        // The first pattern will cut the end of a url and the following item
+        // apart, and the second pattern is to cut apart the 'rel' and the url
+        // following it.
+        // Trim excess whitespace from each of the split items.
+        // Remove the beginning '<' from the URLs.
+        var links = link.split(/>;|",/).map(function (str) {
+            return str.trim();
+        }).map(function (str) {
+            if (/^</.test(str)) {
+                return str.split(/^</)[1];
+            }
+            else {
+                return str;
+            }
+        });
+        // 0 - url
+        // 1 - next | first
+        // 2 - url
+        // 3 - last | prev
+
+        // Get 2nd element from exec call, the capture from the regex
+        var next = /rel="(\w+)/.exec(links[1])[1];
+        if (next === "next") {
+            // Continue on with searches
+            var query = url.parse(links[0]).query;
+            var page = qs.parse(query).page;
+            return page;
+        }
+    }
+
     var results = JSON.parse(body);
+    var linkHeader = response.headers.link;
 
-    //console.log(results.items.length);
+    var completeRepos = results.items;
+    var simpleRepos = completeRepos.map(getSimpleRepoAttributes);
 
-    repos = results.items;
-    var stuff = repos.map(getRepoAttributes);
-    return response.headers.link;
+    var repos = {
+        "complete": completeRepos,
+        "simple": simpleRepos
+    };
+
+    db.push(repos);
+
+    var page = nextPage(linkHeader);
+    if (page) {
+        // console.log("next: " + page);
+        searchGithub(page);
+    }
+    else {
+        callbackNoMore();
+    }
 }
 
-var request = request.defaults({
-    "auth": auth,
-    headers: {
-        "User-Agent": "Rust-Manifest"
-    }
-});
+function searchGithub (page) {
+    // Can only get 100 items max per request.
+    // In the headers, there's a link attribute which lets you know
+    // where you are in the set of results.
+    var MAX_RESULTS = 100;
 
-var test = request({
-    method: 'GET',
-    uri: github+method,
-    "qs": query
-}, callback);
-console.log(test[1]);
+    var query = {
+        q: searchCriteria,
+        per_page: MAX_RESULTS,
+        "page": page,
+    };
+
+    request({
+        method: 'GET',
+        uri: github_api,
+        "qs": query
+    }, processSearchResults);
+}
+
+// Do stuff after all going through all of the search pages.
+function callbackNoMore() {
+    var simple = [],
+        complete = [];
+
+    for (var i = 0; i < db.length; ++i) {
+        console.log(i);
+        simple = simple.concat(db[i].simple);
+        complete = complete.concat(db[i].complete);
+    }
+
+    fs.writeFile("simple_repos.json", JSON.stringify(simple, null, 2), function(err) {
+        if(err) {
+            console.log(err);
+        } else {
+            console.log("simple_repos.json file was saved!");
+        }
+    });
+    fs.writeFile("complete_repos.json", JSON.stringify(complete, null, 2), function(err) {
+        if(err) {
+            console.log(err);
+        } else {
+            console.log("complete_repos.json file was saved!");
+        }
+    });
+}
+
+// Start searching from the first page.
+searchGithub(1);
